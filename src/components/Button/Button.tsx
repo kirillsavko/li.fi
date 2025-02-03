0735ac813c056815fbfa499da3e5c359f6eddb74
Kirill Savko <kirillsavko25@gmail.com>
2025-02-03 19:58:54 +0100

    Adds Button component
    The commit adds the Button component that can be used in the entire application to have all buttons consistent.
    It required some adding:
    1) Adds a new Spinner component that has just a spinner image that is constantly rotating;
    2) Adds a new package for devDependencies that allows us to use an SVG icon as a react component;
    3) Extends the vite configuration by using the new library mentioned above.
    4) Adds a package that allows us using scss instead of css. It has some logic like mixins and functions that css doesn't have;
    5) Updates styles of App.tsx. It's now scss and not css. Also, it has some default styling for the application and css variables. In my opinion, css variables are better than scss because they don't need to be imported in all scss files where they are used and they can be used in JSX inline styles.

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+
+import { Spinner } from '../Spinner/Spinner'
+
+import './Button.scss'
+
+/**
+ * Represents props of {@link Button}
+ */
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  /**
+   * Indicates if the button should have a loading spinner animation that overlaps everything
+   * in the button
+   * @default `undefined` which means there is no loading icon by default
+   */
+  loading?: boolean
+}
+
+/**
+ * Button component of the project. Can be used, for example, for a click action
+ */
+export const Button: FC<ButtonProps> = ({ loading, ...rest }) => {
+  return (
+    <button {...rest} className={`button ${loading ? 'button--loading' : ''} ${rest.className || ''}`}>
+      {rest.children}
+      {loading && <Spinner className='button__spinner' />}
+    </button>
+  )
+}
---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+
+import SpinnerIcon from '../../assets/icons/spinner.svg?react'
+
+import './Spinner.scss'
+
+/**
+ * Represents props for {@link Spinner}
+ */
+type SpinnerProps = {
+  /**
+   * Class name for the component
+   * @default `undefined` which means no class name is applied by default
+   */
+  className?: string
+}
+
+/**
+ * Renders the spinner icon that infinitely rotating. The icon nested the color from the parent
+ */
+export const Spinner: FC<SpinnerProps> = props => {
+  return (
+    <SpinnerIcon className={`spinner ${props.className || ''}`} />
+  )
+}
